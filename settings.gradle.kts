91f161f24c0310efcb4d1b654f4141823e965380
Shibo14 <samsung98abbos@gmail.com>
2023-09-25 11:30:49 +0500

    test

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "12-school Admin Panel"
+include(":app")
+ 
